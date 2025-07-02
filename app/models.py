87a2fe0482ba332a895c25417062c9f0d4406538
Owen <owenkaris9512@gmail.com>
2025-07-02 16:25:39 +0300

    using live backend link on render

--- app/models.py
+++ app/models.py
@@ -30,7 +30,7 @@ class Task(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
--
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
